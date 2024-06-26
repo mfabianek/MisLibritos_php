324169120b9d1d3702bb65aa637f233a6bf2d4e4
zzoip <zpazos@fi.uba.ar>
2024-06-26 20:47:19 -0300

    Script de mostrar contra

--- js/mostrar_contra.js
+++ js/mostrar_contra.js
@@ -1,10 +1,15 @@
 const passwordField = document.getElementById("password");
 const togglePass = document.getElementById("boton_mostrar");
+const toggleEye = document.getElementById("pass_toggle_eye")
 
 togglePass.addEventListener("click", function (){
     if (passwordField.type === "password") {
     passwordField.type = "text";
+    toggleEye.classList.remove("fa-eye-slash");
+    toggleEye.classList.add("fa-eye");
     } else {
     passwordField.type = "password";
+    toggleEye.classList.remove("fa-eye");
+    toggleEye.classList.add("fa-eye-slash");
     }
 })
